0d53300b9d2ae14501fb47227892b33b111b535c
ElRaptorus <christian.werner@5minds.de>
2018-07-10 12:07:32 +0200

    Implement repository initialization

--- src/flow_node_instance_repository.ts
+++ src/flow_node_instance_repository.ts
@@ -1,6 +1,37 @@
+import {getConnection} from '@essential-projects/sequelize_connection_manager';
 import {IFlowNodeInstanceRepository, Runtime} from '@process-engine/process_engine_contracts';
 
+import * as Sequelize from 'sequelize';
+
+import {loadModels} from './model_loader';
+import {FlowNodeInstance as FlowNodeInstanceModel, ProcessToken} from './schemas';
+
 export class FlowNodeInstanceRepository implements IFlowNodeInstanceRepository {
+
+  public config: any;
+  // {
+  //   "username": "admin",
+  //   "password": "admin",
+  //   "database": "processengine",
+  //   "host": "localhost",
+  //   "port": 45678,
+  //   "dialect": "postgres",
+  //   "supportBigNumbers": true,
+  //   "resetPasswordRequestTimeToLive": 12
+  // }
+
+  private _flowNodeInstanceModel: FlowNodeInstanceModel;
+
+  private sequelize: Sequelize.Sequelize;
+
+  private flowNodeInstanceModel(): FlowNodeInstanceModel {
+    return this._flowNodeInstanceModel;
+  }
+
+  public async initialize(): Promise<void> {
+    this.sequelize = await getConnection(this.config.database, this.config.username, this.config.password, this.config);
+    await loadModels(this.sequelize);
+  }
 
   public async queryByCorrelation(correlationId: string): Promise<Array<Runtime.Types.FlowNodeInstance>> {
     throw new Error('Not implemented.');
