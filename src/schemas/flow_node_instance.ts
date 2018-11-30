5913795a0d4e82ab0f150be8c6bda67a30352e20
robpal94 <robin.palkovits@5minds.de>
2018-11-30 11:17:35 +0100

    :recycle: move properties from the processtoken to the fni

--- src/schemas/flow_node_instance.ts
+++ src/schemas/flow_node_instance.ts
@@ -7,9 +7,14 @@ export interface IFlowNodeInstanceAttributes {
   flowNodeInstanceId: string;
   flowNodeId: string;
   flowNodeType: string;
+  eventType?: string;
+  correlationId: string;
+  processModelId: string;
+  processInstanceId: string;
+  identity: object;
+  parentProcessInstanceId?: "string";
   state: Runtime.Types.FlowNodeInstanceState;
   error?: string;
-  eventType?: string;
   // Contains the association to the ProcessToken model.
   // Must be optional, otherwise this property will be expected in the attribute payload of `sequelize.define`.
   processTokens?: Array<ProcessToken>;
@@ -36,16 +41,38 @@ export function defineFlowNodeInstance(sequelize: Sequelize.Sequelize): Sequeliz
       type: Sequelize.STRING,
       allowNull: true, // With regards to old databases, null values must be allowed here
     },
+    eventType: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    correlationId: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    processModelId: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    processInstanceId: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    parentProcessInstanceId: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    identity: {
+      // Since the identity is also a serialised object, we should use TEXT instead of string here.
+      // See ProcessToken.Payload.
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
     state: {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: 0,
     },
     error: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    eventType: {
       type: Sequelize.STRING,
       allowNull: true,
     },
